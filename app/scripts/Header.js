19e96dbe348922063b989ac41a20224fb0318543
Brandon Patterson <Brandon Patterson>
2017-03-10 11:25:56 -0600

    added shop-now-slide to Routes component

--- app/scripts/Header.js
+++ app/scripts/Header.js
@@ -8,7 +8,7 @@ const logoHeader = 'public/images/goldilocksheader.gif'
 const logoBear = 'public/images/goldilocksbear.gif'
 
 const styles = {
-  transition: 'all 0.6s ease-in'
+  transition: 'all 0.45s ease-in'
 }
 
 export default class Header extends React.Component {
@@ -23,7 +23,7 @@ export default class Header extends React.Component {
       paddingTop: 45,
       scale: 1
     }
-    this.onHide = this.onHide.bind(this)
+    this.handleScroll = this.handleScroll.bind(this)
     this.onDropBtnSelect = this.onDropBtnSelect.bind(this)
     this.onNavSelect = this.onNavSelect.bind(this)
   }
@@ -48,61 +48,73 @@ export default class Header extends React.Component {
     }
   }
 
-  onHide () {
-    this.setState({
-      height: 70,
-      logo: logoBear,
-      opacity: 1,
-      paddingTop: 10,
-      top: -13,
-      scale: 0.5
-    })
+  handleScroll () {
+    if (document.body.scrollTop > 1) {
+      this.setState({
+        height: 70,
+        logo: logoBear,
+        opacity: 1,
+        paddingTop: 10,
+        top: -13,
+        scale: 0.5
+      })
+    } else {
+      this.setState({
+        height: 200,
+        logo: logoHeader,
+        opacity: 0,
+        paddingTop: 45,
+        top: 40,
+        scale: 1
+      })
+    }
+  }
+
+  componentWillMount () {
+    window.addEventListener('scroll', this.handleScroll)
   }
 
   render () {
     return (
       <div className='header'>
         <div
+          className='header-background'
           style={{
             ...styles,
             height: this.state.height,
             opacity: this.state.opacity
           }}
-          className='header-background'
         />
-        <div className='header-content'>
-          <div
-            style={{
-              ...styles,
-              paddingTop: this.state.paddingTop
-            }}
-            className={'drop-btn ' + (this.state.transform ? 'drop-btn-transform' : '')}
-            onClick={this.onDropBtnSelect}
-          >
-            <div className='bar1' />
-            <div className='bar2' />
-            <div className='bar3' />
-          </div>
-          <img
-            style={{
-              ...styles,
-              top: this.state.top,
-              transform: 'scale(' + this.state.scale + ')'
-            }}
-            className='logo'
-            onClick={this.onHide}
-            src={this.state.logo}
-            alt='Goldilocks Bear'
-          />
-          <ul
-            style={{
-              paddingTop: this.state.paddingTop
-            }}
-            className={'nav-links' + (this.state.transform ? '-visible' : '')}
-            onClick={this.onNavSelect}
-          >
-            <Routes />
-          </ul>
+        <div
+          className={'drop-btn ' + (this.state.transform ? 'drop-btn-transform' : '')}
+          style={{
+            ...styles,
+            paddingTop: this.state.paddingTop
+          }}
+          onClick={this.onDropBtnSelect}
+        >
+          <div className='bar1' />
+          <div className='bar2' />
+          <div className='bar3' />
+        </div>
+        <img
+          className='logo'
+          style={{
+            ...styles,
+            top: this.state.top,
+            transform: 'scale(' + this.state.scale + ')'
+          }}
+          src={this.state.logo}
+          alt='Goldilocks Bear'
+        />
+        <div
+          className={'nav-links' + (this.state.transform ? '-visible' : '')}
+          style={{
+            paddingTop: this.state.paddingTop
+          }}
+          onClick={this.onNavSelect}
+        >
+          <Routes />
         </div>
       </div>
     )
--- app/scripts/Routes.js
+++ app/scripts/Routes.js
@@ -1,16 +1,47 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router'
 
-const Routes = () => {
-  return (
-    <div >
-      <li><Link to='/'>HOME</Link></li>
-      <li><Link to='about'>ABOUT</Link></li>
-      <li><Link to='#0'>GALLERY</Link></li>
-      <li><Link to='#0'>CONTACT</Link></li>
-      <li><Link to='#0'>SHOP NOW</Link></li>
-    </div>
-  )
+const styles = {
+  transition: 'all 0.45s ease-in'
 }
 
-export default Routes
+export default class Routes extends Component {
+  constructor () {
+    super()
+    this.state = {
+      transform: false
+    }
+    this.handleScroll = this.handleScroll.bind(this)
+  }
+
+  handleScroll () {
+    if (document.body.scrollTop > 1) {
+      this.setState({
+        transform: true
+      })
+    } else {
+      this.setState({
+        transform: false
+      })
+    }
+  }
+
+  componentWillMount () {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  render () {
+    return (
+      <ul>
+        <li><Link to='/'>HOME</Link></li>
+        <li><Link to='about'>ABOUT</Link></li>
+        <li><Link to='#0'>GALLERY</Link></li>
+        <li><Link to='#0'>CONTACT</Link></li>
+        <li
+          className={(this.state.transform ? 'shop-now-slide' : '')}
+          style={{...styles}}
+        ><Link to='#0'>SHOP NOW</Link></li>
+      </ul>
+    )
+  }
+}
