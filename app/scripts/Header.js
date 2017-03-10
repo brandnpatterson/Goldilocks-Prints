a3ef6a45ff725eaa17a9865d3a84c167fdc65f37
Brandon Patterson <Brandon Patterson>
2017-03-10 09:25:18 -0600

    Finished basic transition for header

--- app/scripts/Header.js
+++ app/scripts/Header.js
@@ -7,13 +7,23 @@ import Routes from './Routes'
 const logoHeader = 'public/images/goldilocksheader.gif'
 const logoBear = 'public/images/goldilocksbear.gif'
 
+const styles = {
+  transition: 'all 0.6s ease-in'
+}
+
 export default class Header extends React.Component {
   constructor () {
     super()
     this.state = {
       transform: false,
-      logo: logoHeader
+      height: 200,
+      top: 40,
+      logo: logoHeader,
+      opacity: 0,
+      paddingTop: 45,
+      scale: 1
     }
+    this.onHide = this.onHide.bind(this)
     this.onDropBtnSelect = this.onDropBtnSelect.bind(this)
     this.onNavSelect = this.onNavSelect.bind(this)
   }
@@ -21,13 +31,11 @@ export default class Header extends React.Component {
   onDropBtnSelect () {
     if (this.state.transform === false) {
       this.setState({
-        transform: true,
-        logo: logoBear
+        transform: true
       })
     } else if (this.state.transform === true) {
       this.setState({
-        transform: false,
-        logo: logoHeader
+        transform: false
       })
     }
   }
@@ -38,32 +46,58 @@ export default class Header extends React.Component {
         transform: false
       })
     }
-    if (this.state.logo === logoBear) {
-      this.setState({
-        logo: logoHeader
-      })
-    }
+  }
+
+  onHide () {
+    this.setState({
+      height: 70,
+      logo: logoBear,
+      opacity: 1,
+      paddingTop: 10,
+      top: -13,
+      scale: 0.5
+    })
   }
 
   render () {
     return (
       <div className='header'>
-        <div className='header-background' />
+        <div
+          style={{
+            ...styles,
+            height: this.state.height,
+            opacity: this.state.opacity
+          }}
+          className='header-background'
+        />
         <div className='header-content'>
           <div
-            onClick={this.onDropBtnSelect}
+            style={{
+              ...styles,
+              paddingTop: this.state.paddingTop
+            }}
             className={'drop-btn ' + (this.state.transform ? 'drop-btn-transform' : '')}
+            onClick={this.onDropBtnSelect}
           >
             <div className='bar1' />
             <div className='bar2' />
             <div className='bar3' />
           </div>
           <img
-            src={this.state.logo}
+            style={{
+              ...styles,
+              top: this.state.top,
+              transform: 'scale(' + this.state.scale + ')'
+            }}
             className='logo'
+            onClick={this.onHide}
+            src={this.state.logo}
             alt='Goldilocks Bear'
           />
           <ul
+            style={{
+              paddingTop: this.state.paddingTop
+            }}
             className={'nav-links' + (this.state.transform ? '-visible' : '')}
             onClick={this.onNavSelect}
           >
--- app/scripts/Header/Logo.js
+++ app/scripts/Header/Logo.js
@@ -3,18 +3,31 @@ import React from 'react'
 const logoHeader = 'public/images/goldilocksheader.gif'
 const logoBear = 'public/images/goldilocksbear.gif'
 
+const styles = {
+  transition: 'all 0.6s ease-in'
+}
+
 export default class DropBtn extends React.Component {
   constructor () {
     super()
     this.state = {
-      transform: false,
       logo: logoHeader
     }
   }
 
   render () {
     return (
-
+      <img
+        style={{
+          ...styles,
+          top: this.state.top,
+          transform: 'scale(' + this.state.scale + ')'
+        }}
+        className='logo'
+        onClick={this.onHide}
+        src={this.state.logo}
+        alt='Goldilocks Bear'
+      />
     )
   }
 }
