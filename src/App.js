cdc164df7d35fc6f26d0925c8837b28c18b2041c
Brandon Patterson <brandonnpatterson@gmail.com>
2017-04-18 13:05:47 -0500

    set basic for pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router'
+import Footer from './components/Footer'
 import Header from './components/Header'
 import Routes from './routes/Routes'
 import './App.sass'
@@ -12,7 +13,7 @@ const App = () => {
         <div>
           <Header />
           <Routes />
-          {/* <Footer /> */}
+          <Footer />
         </div>
       </BrowserRouter>
     </div>
