853237dc8eee50e2b7d7727bd4e4e4ca4ce875e1
Brandon Patterson <brandonnpatterson@gmail.com>
2017-04-18 17:22:51 -0500

    react-router-dom added completely

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 // components
 import Footer from './components/Footer'
 import Header from './components/Header'
