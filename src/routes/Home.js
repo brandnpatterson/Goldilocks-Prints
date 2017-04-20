4f3ac352a558b7463da13d292123434e28bce02f
Brandon Patterson <brandonnpatterson@gmail.com>
2017-04-20 10:46:26 -0500

    added down arrow for home page

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,6 +4,19 @@ import { Link } from 'react-router-dom'
 const Home = () => {
   return (
     <div className='home'>
+      <a href='#home'>
+        <div className='arrow'>
+          <div className='arrow-right' />
+          <div className='arrow-left' />
+        </div>
+      </a>
+      <div id='home' className='panel-white'>
+        <ul className='flex-container'>
+          <li><Link to='/art'>ART</Link></li>
+          <li><Link to='/photography'>PHOTOGRAPHY</Link></li>
+          <li><Link to='/design'>DESIGN</Link></li>
+        </ul>
+      </div>
       <div className='panel-white'>
         <ul className='flex-container'>
           <li><Link to='/art'>ART</Link></li>
